fc275d9ea0810277fa9ff833f139ca61420c2fb4
Imanity <imanity14@126.com>
2018-12-28 06:31:43 +0800

    Finished.

--- distance.py
+++ distance.py
@@ -4,5 +4,39 @@ def euler(a, b):
     dis = 0
     for i in range(0, len(a)):
         dis += (a[i] - b[i]) * (a[i] - b[i])
-    dis = math.sqrt(dis)
+    dis = math.sqrt(dis / len(a))
     return dis
+
+def cosine(a, b):
+    aTb = 0
+    al = 0
+    bl = 0
+    for i in range(0, len(a)):
+        aTb += a[i] * b[i]
+        al += a[i] * a[i]
+        bl += b[i] * b[i]
+    discos = aTb / (math.sqrt(al) * math.sqrt(bl))
+    if discos > 1.0:
+        discos = 1.0
+    if discos < -1.0:
+        discos = -1.0
+    dis = math.acos(discos)
+    return dis
+
+def pearson(a, b):
+    a_avg = 0
+    b_avg = 0
+    for i in range(0, len(a)):
+        a_avg += a[i]
+        b_avg += b[i]
+    a_avg /= len(a)
+    b_avg /= len(b)
+    cov = 0
+    da = 0
+    db = 0
+    for i in range(0, len(a)):
+        cov += (a[i] - a_avg) * (b[i] - b_avg)
+        da += (a[i] - a_avg) * (a[i] - a_avg)
+        db += (b[i] - b_avg) * (b[i] - b_avg)
+    pab = cov / (math.sqrt(da) * math.sqrt(db))
+    return 1 - pab
--- main.py
+++ main.py
@@ -8,28 +8,38 @@ import time
 
 dataset_path = 'Dataset/Frogs_MFCCs.csv'
 dataset_3_path = 'Dataset/pca_result/Frogs_MFCCs_pca_3.csv'
+dataset_5_path = 'Dataset/pca_result/Frogs_MFCCs_pca_5.csv'
 
-def cluster_test(dataset, method, arg1, arg2 = 0):
+def cluster_test(dataset, method, arg1, arg2):
     for data in dataset.data:
         data.cluster = 0
     print('===== ' + method + ' =====')
     time_start = time.time()
     if method == 'kmeans':
-        kmeansCluster(dataset, distance.euler, arg1)
+        kmeansCluster(dataset, arg1, arg2)
     elif method == 'bisectingkmeans':
-        bisectingkmeansCluster(dataset, distance.euler, arg1)
+        bisectingkmeansCluster(dataset, arg1, arg2)
     elif method == 'clique':
         cliqueCluster(dataset, arg1, arg2)
     time_end = time.time()
     print('    Time cost: ', time_end - time_start)
     print('    Purity: ', purity(dataset))
-    #print('    F-score: ', fscore(dataset))
+    print('    F-score: ', fscore(dataset))
 
 if __name__ == "__main__":
+    # Read Dataset
     dataset = Dataset()
     dataset.readFromFile(dataset_path, ['MFCCs_' + str(x).rjust(2) for x in list(range(1, 23))], 'Family')
 
+    # Read PCA Dataset
     dataset3 = Dataset()
     dataset3.readFromFile(dataset_3_path, ['MFCCs_' + str(x).rjust(2) for x in list(range(1, 4))], 'Family')
     
-    cluster_test(dataset3, 'clique', 0.1, 5)
+    # K-means
+    cluster_test(dataset, 'kmeans', distance.euler, 4)
+
+    # Bisecting K-means
+    cluster_test(dataset, 'bisectingkmeans', distance.euler, 4)
+
+    # CLIQUE
+    cluster_test(dataset3, 'clique', 1.2, 4)
